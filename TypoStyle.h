70c8f2d33da4e21d517925f17fa99eb4c4e3d621
Neil <spinal_cord@yahoo.com>
2021-02-17 11:49:45 +0000

    Update TypoStyle.h

--- TypoStyle.h
+++ TypoStyle.h
@@ -7,9 +7,9 @@
 class TypoStyle : public Watchy{
     public:
         TypoStyle();
-		void TypoStyle::drawPixel(int16_t x, int16_t y,uint16_t col);
-		void TypoStyle::drawBitmapCol(int16_t x, int16_t y, const uint8_t *bitmap, int16_t w, int16_t h, uint16_t color1, uint16_t color2);
-		void TypoStyle::drawMyRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color1);
+		void drawPixel(int16_t x, int16_t y,uint16_t col);
+		void drawBitmapCol(int16_t x, int16_t y, const uint8_t *bitmap, int16_t w, int16_t h, uint16_t color1, uint16_t color2);
+		void drawMyRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color1);
 		void drawWatchFace();
 };
 
